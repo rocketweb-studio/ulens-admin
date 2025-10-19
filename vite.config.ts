df1c121eb28164afdb0ab0e58afd940f77a545f3
YuryR <myaccount135@gmail.com>
2025-10-19 19:01:20 +0300

    ref vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['admin.ulens.org', '.ulens.org']
+  }
 })
