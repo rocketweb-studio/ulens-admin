09e431684e2d0d086358e979ebd9eab2caf0c408
YuryR <myaccount135@gmail.com>
2025-10-21 15:42:06 +0300

    feat: Header.tsx

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,27 +1,32 @@
+import { PATH } from "@/shared";
+import {IconArrowIosDownOutline, IconFlagUnitedKingdom} from "@rocketweb-studio/ulens-ui-kit";
+import {Link} from "react-router";
+
 export const Header = () => {
   return (
-    <header className="h-[60px] border-b border-[var(--color-dark-300)] px-15 py-3">
+    <header className="h-[60px] border-b border-[var(--color-dark-300)] px-[60px] py-3">
 
-        <div className='flex justify-between items-center'>
-
-          <div>
-            <span className='large '>Ulens</span>
-            <span className='small_text'>Super</span>
-            <span className='semi_bold_small_text'>Admin</span>
-          </div>
-          <div>
-            <select name="" id="">
-              <option value="">English</option>
-              <option value="">Russian</option>
-            </select>
-          </div>
-
+      <div className='flex justify-between items-center' >
+        <div>
+          <span className='large '>Ulens</span>
+          <span className='small_text'>Super</span>
+          <span className='semi_bold_small_text'>Admin</span>
         </div>
+
+        <div className='flex gap-2'> ссылки времено для навигации - <Link to={PATH.main}>main</Link>
+          <Link to={PATH.userList}>users</Link>
+          <Link to={PATH.statistics}>statistics</Link>
+          <Link to={PATH.paymentsList}>payments</Link>
+          <Link to={PATH.postsList}>posts</Link>
+        </div>
+
+        <div className='regular_text_16 h-[36px] border-1 border-[var(--color-dark-300)] flex items-center p-[12px]'>
+          <IconFlagUnitedKingdom/><span className='pl-[12px] pr-[28px]'>English</span><IconArrowIosDownOutline/>
+        </div>
+      </div>
     </header>
   );
 };
-
-
 
 
 // <div>
