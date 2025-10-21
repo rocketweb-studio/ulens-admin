f1655f8a744a225209ada581f8f749729aa748e6
YuryR <myaccount135@gmail.com>
2025-10-21 15:10:39 +0300

    feat: add typography.css colors.css

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,10 +2,10 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
-import './global.css'
+import './style/global.css'
 import '@rocketweb-studio/ulens-ui-kit/dist/index.css'
 import {RouterProvider} from "react-router";
-import {router} from "./app/routes/routes.ts";
+import {router} from "./routes/routes.ts";
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: import.meta.env.VITE_BASE_URL }),
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,18 +1,37 @@
-import {Link} from "react-router";
-import {PATH} from "@/shared";
-
 export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-      <div className="flex flex-row gap-2 ">
-        <h2>ссылки в хедере временно - </h2>
-        <Link to={PATH.main}>main</Link>
-        <Link to={PATH.userList}>users</Link>
-        <Link to={PATH.statistics}>statistics</Link>
-        <Link to={PATH.paymentsList}>payments</Link>
-        <Link to={PATH.postsList}>posts</Link>
-      </div>
-    </div>
+    <header className="h-[60px] border-b border-[var(--color-dark-300)] px-15 py-3">
+
+        <div className='flex justify-between items-center'>
+
+          <div>
+            <span className='large '>Ulens</span>
+            <span className='small_text'>Super</span>
+            <span className='semi_bold_small_text'>Admin</span>
+          </div>
+          <div>
+            <select name="" id="">
+              <option value="">English</option>
+              <option value="">Russian</option>
+            </select>
+          </div>
+
+        </div>
+    </header>
   );
 };
+
+
+
+
+// <div>
+//   <h1>Header</h1>
+//   <div className="flex flex-row gap-2 ">
+//     <h2>ссылки в хедере временно - </h2>
+//     <Link to={PATH.main}>main</Link>
+//     <Link to={PATH.userList}>users</Link>
+//     <Link to={PATH.statistics}>statistics</Link>
+//     <Link to={PATH.paymentsList}>payments</Link>
+//     <Link to={PATH.postsList}>posts</Link>
+//   </div>
+// </div>
