87afb0d43c234d5795ec71a80e2f61abd0438d69
YuryR <myaccount135@gmail.com>
2025-10-23 01:51:26 +0300

    fix: protected routes with accessToken, disabled button on request

--- src/features/sign-in/ui/SignIn.tsx
+++ src/features/sign-in/ui/SignIn.tsx
@@ -3,7 +3,7 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {type SubmitHandler, useForm} from "react-hook-form";
 import {signInSchemas} from "@/features/sign-in/model/schemas/signInSchemas.ts";
 import {useMutation} from "@apollo/client/react";
-import {Navigate, useNavigate} from "react-router";
+import {useNavigate} from "react-router";
 import {PATH} from "@/shared";
 import {loginAdminQuery} from "@/shared/graphql/queries/loginAdmin.ts";
 
@@ -16,13 +16,7 @@ type Inputs = {
 
 export const SignIn = () => {
   const navigate = useNavigate();
-  const [add, {error}] = useMutation(loginAdminQuery);
-
-  const accessToken = localStorage.getItem('adminAccessToken')
-  if (accessToken) {
-    return <Navigate to={PATH.userList} replace />
-  }
-
+  const [add, {error, loading}] = useMutation(loginAdminQuery);
 
   const {
     register,
@@ -34,6 +28,12 @@ export const SignIn = () => {
     defaultValues: {email: '', password: ''},
   })
 
+  const accessToken = localStorage.getItem('adminAccessToken')
+  if (accessToken) {
+    navigate(PATH.userList, { replace: true })
+  }
+
+
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
 
     try {
@@ -41,7 +41,7 @@ export const SignIn = () => {
       console.log('loginData', loginData?.loginAdmin.adminAccessToken)
       if (loginData?.loginAdmin.adminAccessToken)  localStorage.setItem('adminAccessToken', loginData.loginAdmin.adminAccessToken)
       if (!error && typeof loginData?.loginAdmin.adminAccessToken === 'string') {
-        navigate(PATH.userList)
+        navigate(PATH.userList, { replace: true })
       }
     } catch (e) {
     }
@@ -56,7 +56,7 @@ export const SignIn = () => {
         <Input label={'Email'} register={register} name={'email'} error={errors.email?.message}/>
         <Input className={'mb-[14px]'} label={'Password'} register={register} name={'password'} type={'password'}
                showPasswordToggle error={errors.password?.message}/>
-        <Button>Sign In</Button>
+        <Button disabled={loading}>Sign In</Button>
       </form>
     </Card>
   );
