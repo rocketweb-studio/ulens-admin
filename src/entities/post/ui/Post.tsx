5e640cd54011b4d2bcad9dcc9668320dcd48d309
YuryR <myaccount135@gmail.com>
2025-10-28 16:57:58 +0300

    feat: Post.tsx

--- src/entities/post/ui/Post.tsx
+++ src/entities/post/ui/Post.tsx
@@ -1,7 +1,22 @@
+import {UserAvatar} from "@/entities/userAvatar/ui/UserAvatar.tsx";
+import {timeAgo} from "@/shared/utils/timeAgo.ts";
+import {IconBlock} from "@rocketweb-studio/ulens-ui-kit";
+
 export const Post = () => {
   return (
-    <div>
-      <div className='w-[240px] h-[240px] bg-amber-950 ' > photo</div>
+    <div className='flex flex-col gap-[12px] w-[240px]'>
+      <div className='w-[240px] h-[240px] bg-amber-950 '> photo</div>
+      <div className='flex items-center justify-between '>
+        <div className='flex items-center gap-[12px] '>
+          <UserAvatar userName={'yr'} mode={'size'} width={36} height={36}/>
+          <span> Имя </span>
+        </div>
+        <IconBlock className='cursor-pointer'/>
+      </div>
+      <p className='small_text text-[var(--color-light-900)]'>{timeAgo(new Date().toString())}</p>
+      <p className='regular_text_14 w-[235px] h-[72px] overflow-hidden line-clamp-3'>Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Blanditiis ducimus eligendi, eos esse est eum hic neque odio odit omnis pariatur
+        quo recusandae repellat unde, ut vero, voluptas voluptatem voluptates!</p>
     </div>
   );
 };
---
+++ src/entities/userAvatar/ui/UserAvatar.tsx
@@ -0,0 +1,26 @@
+
+type Props = {
+  width?: number
+  height?: number
+  avatarOwner?: string | null
+  userName: string
+  mode: 'fill' | 'size'
+}
+export const UserAvatar = ({ userName = '', mode, avatarOwner, height = 0, width = 0 }: Props) => {
+  const viewMode = mode === 'fill' ? { fill: true, style: { objectFit: 'cover' as const } } : { height, width }
+  return (
+    <>
+      {avatarOwner ?
+        <img
+          className='w-[36px] h-[36px] rounded-[50%] object-cover]'
+          src={`${import.meta.env.VITE_MEDIA_URL}${avatarOwner}`}
+          alt={'avatar'}
+          {...viewMode}
+        />
+        : <div className='rounded-full object-cover bg-[var(--color-light-900)] flex items-center justify-center' style={{ height, width}}>
+          {String(userName).slice(0, 2).toUpperCase()}
+        </div>
+      }
+    </>
+  )
+}
