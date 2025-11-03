4bf55d502641b4647e4bae83f5d92b8f5e087fc8
Sergei-SLS <lyamin.sls@gmail.com>
2025-11-03 20:49:36 +0500

    feat: add component sortUser

--- src/entities/user/lib/sortUser.ts
+++ src/entities/user/lib/sortUser.ts
@@ -2,9 +2,9 @@
 
 export type UserSortValue =
     | "NAME_ASC"
-| "NAME_DESC"
-| "DATA_NEW"
-| "DATE_OLD";
+    | "NAME_DESC"
+    | "DATA_NEW"
+    | "DATE_OLD";
 
 export function sortUsers(users: User[], sort: UserSortValue): User[] {
     const copy = [...users];
@@ -13,20 +13,20 @@ export function sortUsers(users: User[], sort: UserSortValue): User[] {
         case "NAME_ASC":
             return copy.sort((a, b) => a.userName.localeCompare(b.userName));
 
-            case "NAME_DESC":
-                return copy.sort((a, b) => b.userName.localeCompare(a.userName));
+        case "NAME_DESC":
+            return copy.sort((a, b) => b.userName.localeCompare(a.userName));
 
-                case "DATA_NEW":
-                    return copy.sort((a, b) =>
-                    new Date(b.dateAdded).getTime() - new Date(a.dateAdded).getTime(),
-                    );
+        case "DATA_NEW":
+            return copy.sort((a, b) =>
+                new Date(b.dateAdded).getTime() - new Date(a.dateAdded).getTime(),
+            );
 
         case "DATE_OLD":
             return copy.sort((a, b) =>
-            new Date(a.dateAdded).getTime() - new Date(b.dateAdded).getTime(),
+                new Date(a.dateAdded).getTime() - new Date(b.dateAdded).getTime(),
             );
 
-            default:
-                return copy;
+        default:
+            return copy;
     }
 }
