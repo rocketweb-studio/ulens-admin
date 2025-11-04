a54f87ff3c1fa9eb616ff9802dbc82e9369f2ef2
Sergei-SLS <lyamin.sls@gmail.com>
2025-11-04 20:37:25 +0500

    refactor: sortUser change sort

--- src/entities/user/lib/sortUser.ts
+++ src/entities/user/lib/sortUser.ts
@@ -1,32 +1,27 @@
+import type {UserModel} from "@/shared/graphql/generated/graphql.ts";
 
+export type UserSortValue = "AZ" | "ZA" | "NEW" | "OLD";
 
-export type UserSortValue =
-    | "NAME_ASC"
-    | "NAME_DESC"
-    | "DATA_NEW"
-    | "DATE_OLD";
-
-export function sortUsers(users: User[], sort: UserSortValue): User[] {
-    const copy = [...users];
+export const sortUsers = (users: UserModel[], sort: UserSortValue): UserModel[] => {
 
     switch (sort) {
-        case "NAME_ASC":
-            return copy.sort((a, b) => a.userName.localeCompare(b.userName));
+        case "AZ":
+            return [...users].sort((a, b) => a.userName.localeCompare(b.userName));
 
-        case "NAME_DESC":
-            return copy.sort((a, b) => b.userName.localeCompare(a.userName));
+        case "ZA":
+            return [...users].sort((a, b) => b.userName.localeCompare(a.userName));
 
-        case "DATA_NEW":
-            return copy.sort((a, b) =>
-                new Date(b.dateAdded).getTime() - new Date(a.dateAdded).getTime(),
+        case "NEW":
+            return [...users].sort((a, b) =>
+                new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
             );
 
-        case "DATE_OLD":
-            return copy.sort((a, b) =>
-                new Date(a.dateAdded).getTime() - new Date(b.dateAdded).getTime(),
+        case "OLD":
+            return [...users].sort((a, b) =>
+                new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
             );
 
         default:
-            return copy;
+            return users;
     }
 }
