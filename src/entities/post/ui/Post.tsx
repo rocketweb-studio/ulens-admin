4049645685d86daf4aa86d2b58011df27b022b77
YuryR <myaccount135@gmail.com>
2025-11-05 23:09:23 +0300

    feat: add swiper in Post.tsx

--- src/entities/post/ui/Post.tsx
+++ src/entities/post/ui/Post.tsx
@@ -1,6 +1,6 @@
 import {UserAvatar} from "@/entities/userAvatar/ui/UserAvatar.tsx";
 import {timeAgo} from "@/shared/utils/timeAgo.ts";
-import {IconBlock} from "@rocketweb-studio/ulens-ui-kit";
+import {CustomSwiper, IconBlock} from "@rocketweb-studio/ulens-ui-kit";
 import type {ImageModel, PostModel} from "@/shared/graphql/generated/graphql.ts";
 
 export type PostData = Pick<PostModel, 'id' | 'createdAt' | 'description' | 'avatarOwner' | 'userName'> & {
@@ -17,7 +17,12 @@ export const Post = ({postItem}: Props) => {
 
   return (
     <div className='flex flex-col gap-[12px] w-[240px]'>
-      <img className='w-[240px] h-[240px] object-cover' src={`${import.meta.env.VITE_MEDIA_URL}${postItem.images.medium[0].url}`} alt="post photo"/>
+     <CustomSwiper                   slides={postItem.images.medium.map((image, index) => ({
+       id: index,
+       content: (
+           <img key={postItem.id} className='w-[240px] h-[240px] object-cover' src={`${import.meta.env.VITE_MEDIA_URL}${image.url}`} alt="post photo"/>
+       ),
+     }))}/>
       <div className='flex items-center justify-between '>
         <div className='flex items-center gap-[12px] '>
           <UserAvatar userName={'yr'} avatarOwner={postItem.avatarOwner} mode={'size'} width={36} height={36}/>
