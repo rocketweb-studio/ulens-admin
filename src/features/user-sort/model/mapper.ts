f42296b4a2e52a082de77c12dcb1c0d11fef15f9
Sergei-SLS <lyamin.sls@gmail.com>
2025-11-06 20:54:38 +0500

    feat: add UsersSortSelect in UserList

--- src/features/user-sort/model/mapper.ts
+++ src/features/user-sort/model/mapper.ts
@@ -1,44 +1,40 @@
-// import {type SortDirection} from "@/features/user-sort/model/types.ts";
-//
-//
-// export const mapSortToQuery = (sort: SortDirection) => {
-//
-//     switch (sort) {
-//         case 'AZ':
-//             return { sortBy: 'USER_NAME', sortDirection: 'ASC'};
-//
-//         case 'ZA':
-//             return { sortBy: 'USER_NAME', sortDirection: 'DESC'};
-//
-//         case 'NEW':
-//             return {sortBy: 'CREATED_AT', sortDirection: 'DESC'};
-//
-//         case 'OLD':
-//             return {sortBy: 'CREATED_AT', sortDirection: 'ASC'};
-//
-//             default:
-//                 return {sortBy: 'CREATED_AT', sortDirection: 'DESC'};
-//     }
-// }
-
-import {
-    SortableTransactionFields,
-    SortDirection
-} from "@/shared/graphql/generated/graphql.ts";
+import type { SortDirection, SortabeFieldsForUsers } from "@/shared/graphql/generated/graphql";
 
 export type LocalSort = "AZ" | "ZA" | "NEW" | "OLD";
 
-export const mapSortToQuery = (sort: LocalSort) => {
+export const mapSortToQuery = (sort: LocalSort): {
+    sortBy: SortabeFieldsForUsers;
+    sortDirection: SortDirection;
+} => {
     switch (sort) {
         case "AZ":
-            return { sortBy: "USERNAME" as SortableTransactionFields, sortDirection: "ASC" as SortDirection };
+            return {
+                sortBy: "USER_NAME",
+                sortDirection: "ASC"
+            };
+
         case "ZA":
-            return { sortBy: "USERNAME" as SortableTransactionFields, sortDirection: "DESC" as SortDirection };
+            return {
+                sortBy: "USER_NAME",
+                sortDirection: "DESC"
+            };
+
         case "NEW":
-            return { sortBy: "CREATED_AT" as SortableTransactionFields, sortDirection: "DESC" as SortDirection };
+            return {
+                sortBy: "CREATED_AT",
+                sortDirection: "DESC"
+            };
+
         case "OLD":
-            return { sortBy: "CREATED_AT" as SortableTransactionFields, sortDirection: "ASC" as SortDirection };
+            return {
+                sortBy: "CREATED_AT",
+                sortDirection: "ASC"
+            };
+
         default:
-            return { sortBy: "CREATED_AT" as SortableTransactionFields, sortDirection: "DESC" as SortDirection };
+            return {
+                sortBy: "CREATED_AT",
+                sortDirection: "DESC"
+            };
     }
 };
--- src/features/user-sort/ui/UsersSortSelect.tsx
+++ src/features/user-sort/ui/UsersSortSelect.tsx
@@ -1,26 +1,3 @@
-// import type {SortDirection} from "@/features/user-sort/model/types.ts";
-//
-// type Props = {
-//     value: SortDirection;
-//     onChange: (value: SortDirection) => void;
-// }
-//
-// export const UsersSortSelect = ({value, onChange}: Props) => {
-//     return (
-//         <select
-//         value={value}
-//         onChange={(e) => onChange(e.target.value as SortDirection)}
-//         style={{ marginBottom: '16px' }}
-//         >
-//             <option value="AZ">Name: A → Z</option>
-//             <option value="ZA">Name: Z → A</option>
-//             <option value="NEW">Date: New → Old</option>
-//             <option value="OLD">Date: Old → New</option>
-//         </select>
-//     )
-// }
-
-
 import type {LocalSort} from "@/features/user-sort/model/mapper.ts";
 
 type Props = {
